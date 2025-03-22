34862909c2751acade612719e6b7b2fc344a6224
tobiassonoda513 <tobiassonoda513@gmail.com>
2025-03-22 14:48:56 +0000

    Add 17.php

---
+++ src/17.php
@@ -0,0 +1,9 @@
+<?php
+// Sample PHP code for educational purposes only.
+// Do not use this in production code.
+
+$studentName = "John Doe";
+$message = "Hello, $studentName! How are you today?";
+
+echo $message;
+?>
