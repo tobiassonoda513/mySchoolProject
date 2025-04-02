032fd41f6a30eee1103f25126c26731fd61d40e5
tobiassonoda513 <tobiassonoda513@gmail.com>
2025-04-02 15:19:57 +0000

    Add 24.php

---
+++ src/24.php
@@ -0,0 +1,11 @@
+<?php
+// This PHP code snippet is designed to demonstrate how to perform basic operations using PHP variables.
+// It includes some common tasks like adding numbers and printing messages.
+// Example usage:
+
+// Adding two numbers
+echo "Adding 5 and 3: " . (5 + 3) . "\n";
+
+// Printing a message
+echo "Hello, World!";
+?>
