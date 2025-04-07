c49c313ae0e2b3a256371858bd76c6d4de3bbfff
tobiassonoda513 <tobiassonoda513@gmail.com>
2025-04-07 05:30:34 +0000

    Add 32.php

---
+++ src/32.php
@@ -0,0 +1,11 @@
+<?php
+function echo_random_string() {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $length = 10;
+    $random_string = '';
+    for ($i = 0; $i < $length; $i++) {
+        $random_string .= $characters[rand(0, strlen($characters) - 1)];
+    }
+    return $random_string;
+}
+?>
