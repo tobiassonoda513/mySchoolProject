c563a2be691d8e9f88a30637c39ef32c5504c668
tobiassonoda513 <tobiassonoda513@gmail.com>
2025-04-09 19:49:50 +0000

    Add 37.php

---
+++ src/37.php
@@ -0,0 +1,12 @@
+<?php
+// Sample code to demonstrate PHP's various features
+
+class MyClass {
+    public function printMessage() {
+        echo "Hello from MyClass";
+    }
+}
+
+$myObject = new MyClass();
+$myObject->printMessage();
+?>
